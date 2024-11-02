1e80a4e2535fcdc709b88bb6063bf90dd3f36a73
adammo2 <adamobr10@gmail.com>
2024-11-01 23:58:26 -0500

    new click method

--- mealID.py
+++ mealID.py
@@ -1,5 +1,9 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+import time
+
 
 # Set the path to the Chromedriver
 DRIVER_PATH = 'C:\\Users\\adamo\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe'
@@ -11,6 +15,12 @@ driver = webdriver.Chrome(service=service)
 
 # Navigate to the URL
 driver.get('https://eatsmart.housing.illinois.edu/NetNutrition/46')
+
+element = driver.find_element(by = By.LINK_TEXT, value = 'Illinois Street Dining Center (ISR)')
+driver.execute_script("arguments[0].scrollIntoView();", element)
+driver.execute_script("arguments[0].click();", element)
+
+time.sleep(5)
 
 # It's a good practice to close the browser when done
 driver.quit()
