ae38e66eca0e3bf45a69d3fc8a7c2c90c0205282
adammo2 <adamobr10@gmail.com>
2024-11-02 00:57:51 -0500

    new sleenium

--- mealID.py
+++ mealID.py
@@ -2,6 +2,8 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import time
 
 
@@ -16,7 +18,18 @@ driver = webdriver.Chrome(service=service)
 # Navigate to the URL
 driver.get('https://eatsmart.housing.illinois.edu/NetNutrition/46')
 
+time.sleep(3)
 element = driver.find_element(by = By.LINK_TEXT, value = 'Illinois Street Dining Center (ISR)')
+driver.execute_script("arguments[0].scrollIntoView();", element)
+driver.execute_script("arguments[0].click();", element)
+
+time.sleep(3)
+element = driver.find_element(by = By.LINK_TEXT, value = 'Fusion 48')
+driver.execute_script("arguments[0].scrollIntoView();", element)
+driver.execute_script("arguments[0].click();", element)
+
+time.sleep(3)
+element = driver.find_element(by = By.XPATH, value = '//*[@id="cbo_nn_menuDataList"]/div/div[1]/section/div/div/div[1]/a')
 driver.execute_script("arguments[0].scrollIntoView();", element)
 driver.execute_script("arguments[0].click();", element)
 
